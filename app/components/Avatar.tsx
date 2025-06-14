3fac003fccb362b2d9a71c7b2e767eb52bc738f6
chenlinlong <786785040@qq.com>
2025-06-14 17:07:45 +0100

    change font style

--- app/components/Avatar.tsx
+++ app/components/Avatar.tsx
@@ -7,9 +7,7 @@ const Avatar: React.FC<{
 }> = ({
     user
 }) => {
-        const { members } = useActiveList();
-        console.log(members, 'AVATAR------------');
-        
+        const { members } = useActiveList();        
         const isActive = user?.email && (members.indexOf(user.email) !== -1);
         return (
             <div className="relative">
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,14 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist, Geist_Mono, Inter } from "next/font/google";
 import "./globals.css";
 import ToasterContext from "./context/ToasterContext";
 import AuthContext from "./context/AuthContext";
 import ActiveStatus from './components/ActiveStatus';
+
+const inter = Inter({
+  subsets: ['latin'],
+  variable: '--font-inter',
+})
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,7 +33,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${inter.variable} antialiased`}
       >
         <AuthContext>
           <ActiveStatus></ActiveStatus>
